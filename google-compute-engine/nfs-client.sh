8d6dc4b786962749797ccd4d5c732cb7fe7758ca
hamonangann <bornyto.hamonangan@ui.ac.id>
2024-07-06 22:05:28 +0700

    add example script for nfs client/server

---
+++ google-compute-engine/nfs-client.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install nfs-common
+
+sudo mkdir /nfsdata -p
---
+++ google-compute-engine/nfs-server.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install nfs-kernel-server
+
+sudo mkdir /nfsdata -p
+
+sudo echo "/nfsdata *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+sudo systemctl restart nfs-kernel-server
